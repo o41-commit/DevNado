7f68792dee94ac3c4d9e2f261c5a1134e94fe846
o41-commit <oluwatobilobafatoba@gmail.com>
2026-07-15 02:33:54 +0100

    deployment issue fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import MainLayout from "./layout/mainLayot"
-import Index from "./Index"
-import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements } from "react-router-dom"
+import MainLayout from "./layout/MainLayoutt";
+import Index from "./Index";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
 const App = () => {
-
   const router = createBrowserRouter(
     createRoutesFromElements(
       <>
-      <Route path="/" element={<MainLayout/>}>
+        <Route path="/" element={<MainLayout />}>
           <Route index element={<Index />} />
-        
-      </Route>
-      </>
-    )
-  )
+        </Route>
+      </>,
+    ),
+  );
 
-  return< RouterProvider router={router} />
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
